3f60f680b7cbbf20a1f241e9702b6784e7dcd22f
filipkrzyz <krzyzanowski.f@gmail.com>
2019-12-08 18:29:12 +0100

    Initial Commit

---
+++ Classifieds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Classifieds
+//
+//  Created by Filip Krzyzanowski on 08/12/2019.
+//  Copyright © 2019 Filip Krzyzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
